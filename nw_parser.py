c78e10b140a41e636e747bb5e7d10475e08c7758
Aeva Palecek <aeva@lunarfracking.com>
2016-04-03 13:00:29 -0500

    added license headers

--- nw_parser.py
+++ nw_parser.py
@@ -1,10 +1,24 @@
 
+#  Copyright (c) 2016, Aeva M. Palecek
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 import os
 import sys
 import string
 from PIL import Image
-
 
 BASE64 = string.ascii_uppercase + string.ascii_lowercase + string.digits + "+/"
 TILE_SIZE = 16
