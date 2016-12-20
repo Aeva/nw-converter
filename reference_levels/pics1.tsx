ea185efb8df0b2f0952cf3c705fbeeb82cd19601
Aeva Palecek <aeva@lunarfracking.com>
2016-12-19 23:14:27 -0600

    Added some example tiled maps for comparison.

---
+++ reference_levels/pics1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="pics1" tilewidth="16" tileheight="16" tilecount="4096" columns="128">
+ <image source="../sprites/pics1.png" width="2048" height="512"/>
+</tileset>
