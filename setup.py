c7753865530bae8c57999f4945aee8afac3e60f4
Aeva Palecek <aeva.ntsc@gmail.com>
2017-01-06 23:00:38 -0600

    Updated setup.py
    Relates to issue #13 and #15.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import py2exe
 
 dist_dir = os.path.join(os.getcwd(), 'dist')
 if os.path.isdir(dist_dir):
-    shutil.rmtree(dist_dir)
+     shutil.rmtree(dist_dir)
 os.makedirs(dist_dir)
 
 gnome_path = os.path.join(site.getsitepackages()[1], "gnome") 
@@ -22,7 +22,8 @@ gnome_resources = [
     'share\\glib-2.0', 'share\\fonts', 'share\\icons', 'share\\themes\\Default',
     'share\\themes\\HighContrast'
 ]
-gnome_resources += glob.glob(os.path.join(gnome_path, "*.dll"))
+for path in glob.glob(os.path.join(gnome_path, "*.dll")):
+    gnome_resources.append(os.path.split(path)[-1])
 
 def bundle(resource):
     src_path = os.path.join(gnome_path, resource)
@@ -33,14 +34,21 @@ def bundle(resource):
     else:
         shutil.copy(src_path, dest_path)
             
-map(bundle, gnome_resources)
 sys.path.append(dist_dir)
+map(bundle, gnome_resources)
 
 # note, swap "console" and "windows" below to determine if a console
 # window is to be shown
 setup(console=['logger.py'], options={
     'py2exe': {
-        'includes' : ['gi', 'PIL'],
-        'packages': ['gi', 'PIL']
+        'includes' : ['gi', 'PIL.Image'],
+        'packages': ['gi', 'PIL'],
+        'dll_excludes' : ['libgstreamer-1.0-0.dll',
+                          'api-ms-win-core-processthreads-l1-1-2.dll',
+                          'api-ms-win-core-sysinfo-l1-2-1.dll',
+                          'api-ms-win-core-errorhandling-l1-1-1.dll',
+                          'api-ms-win-core-profile-l1-1-0.dll',
+                          'api-ms-win-core-libraryloader-l1-2-0.dll',
+        ],
     }
 })
